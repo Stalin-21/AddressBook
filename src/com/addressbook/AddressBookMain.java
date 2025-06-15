db5918666ba9a0653ba972fdcf91b64539e28f1a
stalin-21 <stalinguna21@gmail.com>
2025-06-15 22:10:06 +0530

    UC5: Added functionality to add more contacts

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -3,33 +3,41 @@ package com.addressbook;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to the Address Book system");
 
         Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
 
-        System.out.println("Enter contact details: ");
+        boolean addMore = true;
 
-        System.out.print("First Name: ");
-        String firstName = sc.nextLine();
-        System.out.print("Last Name: ");
-        String lastName = sc.nextLine();
-        System.out.print("Address: ");
-        String address = sc.nextLine();
-        System.out.print("City: ");
-        String city = sc.nextLine();
-        System.out.print("State: ");
-        String state = sc.nextLine();
-        System.out.print("ZIP: ");
-        String zip = sc.nextLine();
-        System.out.print("Phone Number: ");
-        String phoneNumber = sc.nextLine();
-        System.out.print("Email: ");
-        String email = sc.nextLine();
+        while (addMore) {
+            System.out.println("\nEnter contact details:");
 
-        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        addressBook.addContact(contact);
+            System.out.print("First Name: ");
+            String firstName = sc.nextLine();
+            System.out.print("Last Name: ");
+            String lastName = sc.nextLine();
+            System.out.print("Address: ");
+            String address = sc.nextLine();
+            System.out.print("City: ");
+            String city = sc.nextLine();
+            System.out.print("State: ");
+            String state = sc.nextLine();
+            System.out.print("ZIP: ");
+            String zip = sc.nextLine();
+            System.out.print("Phone Number: ");
+            String phoneNumber = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBook.addContact(contact);
+
+            System.out.print("Do you want to add another contact? (yes/no): ");
+            String response = sc.nextLine();
+            addMore = response.equalsIgnoreCase("yes");
+        }
 
         System.out.println("\nDisplaying all contacts:");
         addressBook.displayDetails();
